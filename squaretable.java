75af6f5ab48a98aaeb893431789a470b78a85d47
preranak1205 <kakadeprerana0512@gmail.com>
2026-03-19 13:25:56 +0530

    experiment 6

---
+++ squaretable.java
@@ -0,0 +1,26 @@
+package experiment6;
+import java.util.Scanner;
+
+public class squaretable 
+{
+
+    public static void main(String[] args) 
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int square = num * num;
+        System.out.println("Square of " + num + " = " + square);
+
+        System.out.println("Multiplication Table:");
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+
+        sc.close();
+    }
+
+}
